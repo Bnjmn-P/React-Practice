cf3b3a80d6aae6011e29ebcd47fb1c6b4a803016
Benjamin501 <144312142+Bnjmn-P@users.noreply.github.com>
2023-09-08 11:50:11 +1200

    Added header and education

---
+++ cv/src/components/Education.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Education = () => {
+  return (
+    <section className="education">
+      <h2 className="section-title">Education</h2>
+      <div className="education-item">
+        <h3 className="education-title">Bachelor of Science in Computer Science</h3>
+        <p className="education-institution">University of Example</p>
+        <p className="education-date">Graduated: May 2020</p>
+      </div>
+      <div className="education-item">
+        <h3 className="education-title">High School Diploma</h3>
+        <p className="education-institution">Example High School</p>
+        <p className="education-date">Graduated: June 2016</p>
+      </div>
+    </section>
+  );
+}
+
+export default Education;
---
+++ cv/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <h1 className="header-title">Benjamin Polglase</h1>
+        <p className="header-subtitle">Software Developer</p>
+        <div className="header-contact">
+          <p>Email: benjaminpolglase.com</p>
+          <p>LinkedIn: www.linkedin.com/in/benjamin-polglase-ba3022275</p>
+          <p>GitHub: https://github.com/Bnjmn-P</p>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
